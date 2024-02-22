e45ee8304743ffaa5cdc82ce3d9d680910087af0
Lautaro Tintez <lautarotintez@gmail.com>
2024-02-22 16:39:09 -0300

    obteniendo productos

--- src/app/components/producto-lista/producto-lista.component.ts
+++ src/app/components/producto-lista/producto-lista.component.ts
@@ -1,10 +1,24 @@
 import { Component } from '@angular/core';
+import { Producto } from 'src/app/models/producto';
+import { ProductoService } from 'src/app/services/producto.service';
 
 @Component({
   selector: 'app-producto-lista',
   templateUrl: './producto-lista.component.html',
-  styleUrls: ['./producto-lista.component.css']
+  styleUrls: ['./producto-lista.component.css'],
 })
 export class ProductoListaComponent {
+  productos: Producto[];
 
+  constructor(private productoServicio: ProductoService) {}
+
+  ngOnInit() {
+    this.obtenerProductos();
+  }
+
+  obtenerProductos() {
+    this.productoServicio.obtenerProductos().subscribe((datos) => {
+      this.productos = datos;
+    });
+  }
 }
